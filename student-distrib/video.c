f1d670a19af5fd83b06c0e17c952242585a5386a
Xinyiguo2 <xinyig2@illinois.edu>
2018-10-28 15:30:40 -0500

    adding comments for video.c

--- student-distrib/video.c
+++ student-distrib/video.c
@@ -7,6 +7,12 @@
 vid_data_t * display;
 terminal_info_t tinfo;
 
+/* video_initialization 
+ * INPUT : none 
+ * OUTPUT :none 
+ * EFFECT : initialze the terminal display to cyan as default 
+ * RETURN : none 
+ */
 void vid_init(){
       int i;
       display = (vid_data_t *)VIDMEM;
@@ -18,6 +24,13 @@ void vid_init(){
       return;
 }
 
+/* fill_color 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT : fill the terminal by color red after first initialilzation 
+ * RETURN : none 
+ */
+
 void fill_color(){
       int i;
       for(i = 0; i < MAXCHAR; i++){
@@ -26,6 +39,13 @@ void fill_color(){
       return;
 }
 
+/* clear_term 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT :called with ctrl+l is hit, wipe the terminal's characters 
+ *         reposition the cursor's place at (0,0)
+ * RETURN :none 
+ */
 void clear_term(){
       int i;
       for(i = 0; i < MAXCHAR; i++){
@@ -35,6 +55,13 @@ void clear_term(){
       return;
 }
 
+/* scroll_term 
+ * DESCRIPTION : called when the terminal is fully written and need to move upwards for one empty line at bottom 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT : shift the display screen up with one empty line at the bottom 
+ * RETURN :none 
+ */
 void scroll_term(){
       int i;
       for(i = TERMWIDTH; i < MAXCHAR; i++){
@@ -47,6 +74,15 @@ void scroll_term(){
       }
 }
 
+/* print_term 
+ * DESCRIPTION : called when 'ENTER' is hit and should display chars typed by user
+ * INPUT : string - character string that needed to be printed 
+ *         length - the length of the string 
+ * OUTPUT : none 
+ * EFFECT : display the characters on the terminal by modifying the display screen 
+ *          check for if needed to scroll if the terminal display is fully written 
+ * RETURN : none 
+ */
 void print_term(uint8_t * string, int length){
       int i;
       for(i = 0; i < length; i++){
@@ -67,6 +103,14 @@ void print_term(uint8_t * string, int length){
       }
 }
 
+/* print_term 
+ * DESCRIPTION : called whenever user typed in a character 
+ * INPUT : a - the character that needed to be printed 
+ * OUTPUT : none 
+ * EFFECT : display the character just typed in by the user 
+ *          check for if needed to scroll if the terminal is fully written 
+ * RETURN : none 
+ */
 void printchar_term(char a){
 
       if(a == '\0'){
@@ -87,6 +131,13 @@ void printchar_term(char a){
       }
 }
 
+/* backspace 
+ * DESCRIPTION : called when user hit 'BACKSPACE' to delete a character 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT : move cursor backward one position , wipe out the last character displated on the terminal 
+ * RETURN : none 
+ */
 void backspace(){
       tinfo.offset--;
       if(tinfo.offset > MAXCHAR){
@@ -96,6 +147,13 @@ void backspace(){
       return;
 }
 
+/* tab 
+ * DESCRIPTION : move the cursor to the corresponding position after hitting tab 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT : move the cursor to the corresponding position after hitting tab 
+ * RETURN : none 
+ */
 void tab(){
       tinfo.offset = (tinfo.offset / 10) + 10;
       if(tinfo.offset > MAXCHAR){
@@ -104,6 +162,13 @@ void tab(){
       return;
 }
 
+/* set_term_x 
+ * DESCRIPTION : move the cursor to the correct character 
+ * INPUT : x - the term where cursor should be at 
+ * OUTPUT : none 
+ * EFFECT : move the cursor to the correct character 
+ * RETURN : none 
+ */
 void set_term_x(uint32_t x){
       if(x > TERMWIDTH){
             return;
@@ -112,7 +177,13 @@ void set_term_x(uint32_t x){
       tinfo.offset += x;
       return;
 }
-
+/* print_num 
+ * DESCRIPTION : print the number of its correct order of characters digits 
+ * INPUT : none 
+ * OUTPUT : none 
+ * EFFECT : print the number of its correct order of characters digits 
+ * RETURN : none 
+ */
 void print_num(int x){
       char num_ascii[100];
       int count = -1;
